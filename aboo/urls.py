0bffb42501e8995071437621faaeb90d428fd7d6
Abooki <93200697+200-4@users.noreply.github.com>
2025-09-20 17:52:29 +0600

    Update urls.py

--- aboo/urls.py
+++ aboo/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('',views.home, name='home'),
     path('projects/', views.projects, name='projects'),
-    path('hire_me', views.hire_me, name='hire_me'),
+    path('hire_me/', views.hire_me, name='hire_me'),
     path('learn_more/<int:service_id>/', views.learn_more, name='learn_more'),
     path('contact/', views.contact, name='contact'),
     #add more urls here
